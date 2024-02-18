bcfd7be2bfea4065703f1d93d355ca6c3da7ee6e
HarshR <harsh.repal2003@gmail.com>
2024-02-18 18:29:07 +0530

    Home Page section1

---
+++ src/components/core/HomePage/CTAButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const CTAButton = ({children,active,linkto}) => {
+  return (
+    <Link to={linkto}>
+        <div className={`text-center text-[16px] px-6 py-3 rounded-lg shadow-[2px_2px_0px_0px_rgba(255,255,255,0.18)] hover:drop-shadow-none
+            ${active ? 'bg-yellow-50 text-black':'bg-richblack-800'  } hover:scale-95 transition-all duration-200
+        `}>
+            {children}
+        </div>
+    </Link>
+)
+}
+
+export default CTAButton
---
+++ src/pages/Home.jsx
@@ -0,0 +1,144 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {FaArrowRight} from "react-icons/fa"
+import HighlightText from '../components/core/HomePage/HighlightText'
+import CTAButton from '../components/core/HomePage/CTAButton'
+import Banner from "../assets/Images/banner.mp4"
+import CodeBlock  from '../components/core/HomePage/CodeBlock'
+
+
+
+
+const Home = () => {
+  return (
+    <div>
+        {/* Section1 */}
+        <div className='relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center justify-between text-white gap-8'>
+            {/* signup Btn */}
+            <Link to={"/signup"}>
+                <div className=' group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200
+                transition-all duration-200 hover:scale-95 w-fit drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] hover:drop-shadow-none'>
+                    {/* div for content */}
+                    <div className='flex items-center gap-2 rounded-full px-10 py-[5px] group-hover:bg-richblack-900'>
+                        <p>Become an Instructor</p>
+                        <FaArrowRight/> 
+                    </div>                   
+                </div>
+            </Link>
+            
+            {/* Headline */}
+            <div className=' text-center text-4xl font-semibold '>
+                Empower your  Future with <HighlightText text={"Coding Skills"} />
+            </div>
+
+            {/* Subheadline */}
+            <div className='-mt-3 w-[90%] text-center text-lg font-bold text-richblack-300 '>
+                With our online coding courses, you can learn at your own pace, from anywhere in the world,
+                and get access to a wealth of resources, including hands-on projects, quizzes, and personalized
+                feedback from instructors. 
+            </div>
+
+            {/* button headsection */}
+            <div className='flex gap-7 mt-8'>
+                <CTAButton active={true} linkto={"/signup"}>
+                    Learn More
+                </CTAButton>
+                <CTAButton active={false} linkto={"/login"}>
+                    Book a Demo
+                </CTAButton>
+
+            </div>
+
+            {/* video */}
+            <div className='mx-3 my-12 shadow-blue-200 w-[77%] relative '>
+                <div className='grad2 -top-10 w-[800px] rounded-full'></div>
+                <video className='video'
+                    muted
+                    loop
+                    autoPlay
+                    >
+                    <source  src={Banner} type="video/mp4" />
+                </video>
+            </div>
+
+            {/* Code Section 1 */}
+            <div>
+                <CodeBlock 
+                position={"lg:flex-row"}
+                heading={
+                    <div className='text-4xl font-semibold'>
+                        Unblock your <HighlightText text={"coding portential"}/>  with our online courses.
+                    </div>
+                }
+                subheading={
+                `    Our courses are designed and taught by industry experts who have years
+                    of experience in coding and are passionate about sharing their knowledge with you.
+                `}
+                ctabtn1={
+                    {
+                        btnText:"try it yourself",
+                        linkto:"/signup",
+                        active:true
+                    }
+                }
+                ctabtn2={
+                    {
+                        btnText:"learn more",
+                        linkto:"/login",
+                        active:false
+                    }
+                }
+                codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
+                backgroundGradient={<div className="codeblock1 absolute"></div>}
+                codeColor={"text-yellow-5"}
+                >
+
+                </CodeBlock>
+            </div>
+            
+            {/* Code Section 2 */}
+            <div className='w-[100%]'>
+                <CodeBlock 
+                position={"lg:flex-row-reverse"}
+                heading={
+                    <div className='text-4xl font-semibold'>
+                        Start <HighlightText text={"coding in seconds"} /> with our online
+                courses.
+                    </div>
+                }
+                subheading={
+                    `Go ahead, give it a try. Our hands-on learning environment 
+                    means you'll be writing real code from your very first lesson.`}
+                ctabtn1={
+                    {
+                        btnText:"Continue Lesson",
+                        linkto:"/signup",
+                        active:true
+                    }
+                }
+                ctabtn2={
+                    {
+                        btnText:"learn more",
+                        linkto:"/login",
+                        active:false
+                    }
+                }
+                codeColor={"text-white"}
+                codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
+                backgroundGradient={<div className="codeblock2 absolute"></div>}
+                >
+
+                </CodeBlock>
+            </div>
+
+        </div>
+
+
+        {/* Section2 */}
+        {/* Section3 */}
+        {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
